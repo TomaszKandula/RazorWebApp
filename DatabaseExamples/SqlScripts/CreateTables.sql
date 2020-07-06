0c05c3041c6c55b298afc317ef6a9ea74fe93e0f
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-07-06 22:56:44 +0200

    add import sql scripts with example data

---
+++ DatabaseExamples/SqlScripts/CreateTables.sql
@@ -0,0 +1,36 @@
+
+CREATE TABLE Countries
+(
+    Id          INT IDENTITY(1,1) NOT NULL,
+    CountryName NVARCHAR(255) NOT NULL,
+    CountryCode VARCHAR(2) NOT NULL,
+    CONSTRAINT PK__Id__Countries PRIMARY KEY (Id)
+)
+
+
+CREATE TABLE Cities
+(
+    Id          INT IDENTITY(1,1) NOT NULL,
+    CountryId   INT NOT NULL,
+    CityName    NVARCHAR(255) NOT NULL,
+    CONSTRAINT PK__Id__Cities PRIMARY KEY (Id),
+    CONSTRAINT FK__CountryId__Cities FOREIGN KEY (CountryId) REFERENCES Countries (Id)
+)
+
+
+CREATE TABLE Users
+(
+    Id          INT IDENTITY(1,1) NOT NULL,
+    CountryId   INT NOT NULL,
+    CityId      INT NOT NULL,
+    FirstName   NVARCHAR(255) NOT NULL,
+    LastName    NVARCHAR(255) NOT NULL,
+    NickName    NVARCHAR(255) NOT NULL,
+    EmailAddr   VARCHAR(255) NOT NULL,
+    PhoneNum    CHAR(15) NULL,
+    Password    VARCHAR(255) NOT NULL,
+    CONSTRAINT PK__Id__Users PRIMARY KEY (Id),
+    CONSTRAINT UQ__EmailAddr__Users UNIQUE (EmailAddr),
+    CONSTRAINT FK__CountryId__Users FOREIGN KEY (CountryId) REFERENCES Countries (Id),
+    CONSTRAINT FK__CityId__Users FOREIGN KEY (CityId) REFERENCES Cities (Id)
+)
