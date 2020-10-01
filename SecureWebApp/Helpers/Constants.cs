73959699448d560833742071da2e20f60e2c4c3c
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-10-01 23:47:01 +0200

    refactoring, align with naming conventions, simplify code when possible

--- SecureWebApp/Helpers/Constants.cs
+++ SecureWebApp/Helpers/Constants.cs
@@ -5,7 +5,8 @@
     /// This class is responsible only for providing constants to all classes/methods etc. accross the application.
     /// It can be a partial class if necessary; and if so, then put the module in the root folder and additional 
     /// partials in other project folders.
-    public class Constants
+    /// </summary>
+    public static class Constants
     {
 
         public static class Sessions 
@@ -14,7 +15,7 @@
             /* Use this value to determin user session timeout */
             public const int IdleTimeout = 15;
 
-            internal class KeyNames 
+            internal static class KeyNames 
             {
                 public const string SessionId = "SessionId";
                 public const string EmailAddr = "EmailAddr";
@@ -26,37 +27,37 @@
         public static class Errors 
         {
 
-            internal class EmailAlreadyExists
+            internal static class EmailAlreadyExists
             {
                 public const string ErrorCode = "email_already_exists";
                 public const string ErrorDesc = "This email address already exists.";
             }
 
-            internal class EmailAddressMalformed
+            internal static class EmailAddressMalformed
             {
                 public const string ErrorCode = "email_is_malformed";
                 public const string ErrorDesc = "This email address is malformed.";
             }
 
-            internal class EmailDomainNotExist
+            internal static class EmailDomainNotExist
             {
                 public const string ErrorCode = "is_email_domain_exist";
                 public const string ErrorDesc = "The email domain is not exist.";
             }
 
-            internal class InvalidPayLoad 
+            internal static class InvalidPayLoad 
             {
                 public const string ErrorCode = "invalid_payload";
                 public const string ErrorDesc = "The received content cannot be validated.";
             }
 
-            internal class InvalidCredentials 
+            internal static class InvalidCredentials 
             {
                 public const string ErrorCode = "invalid_credentials";
                 public const string ErrorDesc = "Supplied email address and/or password is incorrect.";
             }
 
-            internal class AccountNotActivated
+            internal static class AccountNotActivated
             {
                 public const string ErrorCode = "account_not_activated";
                 public const string ErrorDesc = "Your account is not activated.";
