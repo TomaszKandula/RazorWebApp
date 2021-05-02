dc190ef70f321790b6c7a0c6b117e7a97f49786f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-02 12:11:43 +0200

    fix: replace TAB with SPACE (4 indents)

--- Tests/RazorWebApp.TestData/DataProvider.cs
+++ Tests/RazorWebApp.TestData/DataProvider.cs
@@ -7,78 +7,70 @@ namespace RazorWebApp.TestData
 {   
     public static class DataProvider
     {
-		private static Random FRandom = new Random();
+        private static Random FRandom = new Random();
 
-		public static int GetRandomInt(int AMin = 0, int AMax = 12)
-		{
-			return FRandom.Next(AMin, AMax + 1);
-		}
+        public static int GetRandomInt(int AMin = 0, int AMax = 12)
+            => FRandom.Next(AMin, AMax + 1);
+      
+        public static string GetRandomEmail(int ALength = 12, string ADomain = "gmail.com")
+            => $"{GetRandomString(ALength)}@{ADomain}";
 
-		public static string GetRandomEmail(int ALength = 12, string ADomain = "gmail.com")
-		{
-			return $"{GetRandomString(ALength)}@{ADomain}";
-		}
+        public static decimal GetRandomDecimal(int AMin = 0, int AMax = 9999)
+            => FRandom.Next(AMin, AMax);
 
-		public static decimal GetRandomDecimal(int AMin = 0, int AMax = 9999)
-		{
-			return FRandom.Next(AMin, AMax);
-		}
+        public static string GetRandomString(int ALength = 12, string APrefix = "")
+        {
+            if (ALength == 0) 
+                return string.Empty;
 
-		public static string GetRandomString(int ALength = 12, string APrefix = "")
-		{
-			if (ALength == 0) return string.Empty;
-			
-			const string LChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            const string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-			var LString = new string(Enumerable.Repeat(LChars, ALength)
-				.Select(AString => AString[FRandom.Next(AString.Length)])
-				.ToArray());
+            var LString = new string(Enumerable.Repeat(CHARS, ALength)
+                .Select(AString => AString[FRandom.Next(AString.Length)])
+                .ToArray());
 
-			if (!string.IsNullOrEmpty(APrefix) || !string.IsNullOrWhiteSpace(APrefix)) 
-				return APrefix + LString;
+            if (!string.IsNullOrEmpty(APrefix) || !string.IsNullOrWhiteSpace(APrefix)) 
+                return APrefix + LString;
 
-			return LString;
-		}
+            return LString;
+        }
 
-		public static byte[] GetRandomByteArray(int ASizeInKb = 12)
-		{
-			var LByteBuffer = new byte[ASizeInKb * 1024];
-			FRandom.NextBytes(LByteBuffer);
-			return LByteBuffer;
-		}
+        public static MemoryStream GetRandomStreamData(int ASizeInKb = 12)
+            => new MemoryStream(GetRandomByteArray(ASizeInKb));
 
-		public static MemoryStream GetRandomStreamData(int ASizeInKb = 12)
-		{
-			var LByteBuffer = GetRandomByteArray(ASizeInKb);
-			return new MemoryStream(LByteBuffer);
-		}
+        public static DateTime GetRandomDate(DateTime? AMin = null, DateTime? AMax = null, int ADefaultYear = 2020)
+        {
+            AMin ??= new DateTime(ADefaultYear, 1, 1);
+            AMax ??= new DateTime(ADefaultYear, 12, 31);
 
-		public static DateTime GetRandomDate(DateTime? AMin = null, DateTime? AMax = null, int ADefaultYear = 2020)
-		{
-			if (!AMin.HasValue) AMin = new DateTime(ADefaultYear, 1, 1);
-			if (!AMax.HasValue) AMax = new DateTime(ADefaultYear, 12, 31);
+            var LDayRange = (AMax - AMin).Value.Days;
+            return AMin.Value.AddDays(FRandom.Next(0, LDayRange));
+        }
 
-			var LDayRange = (AMax - AMin).Value.Days;
-			return AMin.Value.AddDays(FRandom.Next(0, LDayRange));
-		}
+        public static T GetRandomEnum<T>()
+        {
+            var LRandom = new Random();
+            var LValues = Enum.GetValues(typeof(T));
+            return (T)LValues.GetValue(LRandom.Next(LValues.Length));
+        }
 
-		public static T GetRandomEnum<T>()
-		{
-			var LRandom = new Random();
-			var LValues = Enum.GetValues(typeof(T));
-			return (T)LValues.GetValue(LRandom.Next(LValues.Length));
-		}
+        public static string Base64Encode(string APlainText)
+        {
+            var LPlainTextBytes = Encoding.UTF8.GetBytes(APlainText);
+            return Convert.ToBase64String(LPlainTextBytes);
+        }
 
-		public static string Base64Encode(string APlainText)
-		{
-			var LPlainTextBytes = Encoding.UTF8.GetBytes(APlainText);
-			return Convert.ToBase64String(LPlainTextBytes);
-		}
+        public static string Base64Decode(string ABase64EncodedData)
+        {
+            var LBase64EncodedBytes = Convert.FromBase64String(ABase64EncodedData);
+            return Encoding.UTF8.GetString(LBase64EncodedBytes);
+        }
 
-		public static string Base64Decode(string ABase64EncodedData)
-		{
-			var LBase64EncodedBytes = Convert.FromBase64String(ABase64EncodedData);
-			return Encoding.UTF8.GetString(LBase64EncodedBytes);
-		}
-	}
+        private static byte[] GetRandomByteArray(int ASizeInKb = 12)
+        {
+            var LByteBuffer = new byte[ASizeInKb * 1024];
+            FRandom.NextBytes(LByteBuffer);
+            return LByteBuffer;
+        }
+    }
 }
